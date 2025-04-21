0087fa19147465a75727e952140eca0ebb06b863
Toni <dannyore001@gmail.com>
2025-04-21 12:41:58 +0100

    postgres adapter installed

---
+++ app/app/calc.py
@@ -0,0 +1,11 @@
+"""
+Calculator function
+"""
+
+def add(x, y):
+    """Add two numbers."""
+    return x + y
+
+def subtract(x, y):
+    """Subtract x from y and return result"""
+    return y - x
