f71cd685e6718d2f74e05549a8dc1a633a2eae2f
Toni <dannyore001@gmail.com>
2025-05-05 12:42:26 +0100

    documenation configured

---
+++ app/user/tests/test_user_api.py
@@ -0,0 +1,27 @@
+"""
+Tests for the user API
+"""
+
+from django.test import TestCase
+from django.contrib.auth import get_user_model
+from django.urls import reverse
+
+from rest_framework.test import APIClient
+from rest_framework import status
+
+CREATE_USER_URL = reverse('user:create')
+
+def create_user(**params):
+    """Create and return a new user"""
+    return get_user_model().objects.create_user(**params)
+
+class PublicUserApiTests(TestCase):
+    """Test the public features of the user API"""
+
+    def setUp(self):
+        self.client = APIClient()
+        return super().setUp()
+
+    def test_create_user_success(self):
+        """test creating user is successsful"""
+
