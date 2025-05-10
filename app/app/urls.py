baf2bdb99a123051dfa48a8e6712831ff20870ba
Toni <dannyore001@gmail.com>
2025-05-10 17:13:20 +0100

    auth token service created

--- app/app/urls.py
+++ app/app/urls.py
@@ -16,7 +16,7 @@ Including another URLconf
 
 from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 urlpatterns = [
     path("admin/", admin.site.urls),
@@ -26,4 +26,5 @@ urlpatterns = [
         SpectacularSwaggerView.as_view(url_name="api-schema"),
         name="api-docs",
     ),
+    path("api/user/", include("user.urls")),
 ]
